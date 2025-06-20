1bd3992621a0a5e6ee5acd9eae5e3dee49407544
streamer <t-laichuanliu@antateam.com>
2025-06-20 15:15:55 +0800

    fix: 修改打包脚本:不进行全部打包,只打包对应的需要文件

--- pkg.sh
+++ pkg.sh
@@ -2,7 +2,7 @@ git pull origin master
 
 rm ../ai-diffusion-latest.zip
 
-zip -r  -UN=UTF8 ../ai-diffusion-latest.zip .
+zip -r  -UN=UTF8 ../ai-diffusion-latest.zip ai_diffusion ai_diffusion.desktop
 ossutil cp result.json oss://antaai/comfyui/krita/
 
 cd ..
